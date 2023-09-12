adb8b6d9244eb2fab1953ff029305d0665832656
yicr <yicr@users.noreply.github.com>
2023-09-12 12:07:02 +0900

    chore: projen setting update

--- .projenrc.ts
+++ .projenrc.ts
@@ -7,12 +7,24 @@ const project = new awscdk.AwsCdkConstructLibrary({
   jsiiVersion: '~5.0.0',
   defaultReleaseBranch: 'main',
   name: '@gammarer/aws-waf-managed-rules-set-web-acl',
+  description: 'This is an AWS CDK Construct for AWS Managed Rules Sets Web ACL on WAF V2.',
+  keywords: ['aws', 'cdk', 'aws-cdk', 'waf', 'acl'],
   projenrcTs: true,
   repositoryUrl: 'https://github.com/yicr/aws-waf-managed-rules-set-web-acl.git',
   npmAccess: javascript.NpmAccess.PUBLIC,
   minNodeVersion: '16.0.0',
   workflowNodeVersion: '18.17.1',
-  releaseToNpm: false,
-  depsUpgrade: false,
+  releaseToNpm: true,
+  stability: 'experimental', // stable
+  depsUpgradeOptions: {
+    workflowOptions: {
+      labels: ['auto-approve', 'auto-merge'],
+      schedule: javascript.UpgradeDependenciesSchedule.expressions(['0 19 * * *']),
+    },
+  },
+  autoApproveOptions: {
+    secret: 'GITHUB_TOKEN',
+    allowedUsernames: ['yicr'],
+  },
 });
 project.synth();
